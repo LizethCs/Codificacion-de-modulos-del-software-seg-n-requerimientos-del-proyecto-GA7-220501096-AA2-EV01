b5c711803437938900da19ee93452fee96e2b453
LizethCs <lcastaeda15@soy.sena.edu.co>
2024-07-29 17:30:45 -0500

    add variables

--- src/main/java/com/evidencia/limcasoft/main.java
+++ src/main/java/com/evidencia/limcasoft/main.java
@@ -1,7 +1,18 @@
 package com.evidencia.limcasoft;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class main {
 
     public static void main(String[] args) throws SQLException, SQLException {
- 
+        String user = "root";
+        String password = ""; 
+        String url = "jdbc:mysql://127.0.0.1:3306/db";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+
     }
 }
