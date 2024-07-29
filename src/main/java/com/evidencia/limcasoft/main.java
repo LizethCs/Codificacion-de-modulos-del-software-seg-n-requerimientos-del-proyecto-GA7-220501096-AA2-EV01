69f2aba472e3a01ef773eedf46d15f634710352b
LizethCs <lcastaeda15@soy.sena.edu.co>
2024-07-29 17:45:49 -0500

    add CRUD

--- src/main/java/com/evidencia/limcasoft/main.java
+++ src/main/java/com/evidencia/limcasoft/main.java
@@ -13,6 +13,23 @@ public class main {
         String url = "jdbc:mysql://127.0.0.1:3306/db";
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
-
+        
+        //Create
+        statement.execute("INSERT INTO customers VALUES(NULL, \"Daniel\", \"Carvajal Perez\",\"ex@gmail.com\",\"calle 1 barrio abc\",\"31000000\",\"CC\",\"1020670000\");");
+        //Update
+        statement.execute("UPDATE customers SET name = \"Juan Carlos\", lastname = \"Lopez Soto\" WHERE ID = 1;");
+        //Delete 
+        statement.execute("DELETE FROM customers WHERE ID = 1;");
+        //Read
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM customers");
+        while (resultSet.next()) {
+            System.out.println("\nRegistro #" + String.valueOf(resultSet.getInt("id")) + "\n" + "Nombre: " + resultSet.getString("name")
+                    + " \nApellidos: " + resultSet.getString("lastname")
+                    + " \nEmail: " + resultSet.getString("email")
+                    + " \nDirección: " + resultSet.getString("address")
+                    + " \nNúmero: " + resultSet.getString("number")
+                    + " \nTipo documento: " + resultSet.getString("docType")
+                    + " \nNúmero de documento: " + resultSet.getString("docNumber"));
+        }
     }
 }
